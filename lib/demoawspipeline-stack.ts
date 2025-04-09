09da6301b97dff683dd3c28e42ce8687e4f280d6
intimetecbci <kori.swaroopwork@gmail.com>
2025-04-09 11:15:30 +0530

    final commit

--- lib/demoawspipeline-stack.ts
+++ lib/demoawspipeline-stack.ts
@@ -2,17 +2,13 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import { CodePipeline, CodePipelineSource, ShellStep} from 'aws-cdk-lib/pipelines';
+import { PipelineAppStage } from './demoawspipeline-app-stack';
+import {ManualApprovalStep} from 'aws-cdk-lib/pipelines';
+
 
 export class DemoawspipelineStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'DemoawspipelineQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
 
     const democicdpipeline=new CodePipeline(this,'demopipeline',
       {
@@ -22,7 +18,20 @@ export class DemoawspipelineStack extends cdk.Stack {
 
           ],
         }),
+        pipelineName: "bci-local-test-pipeline"
       });
+
+      const testingStage=democicdpipeline.addStage(new PipelineAppStage(this,'test', {
+        env: { account: '715841370050', region: 'ap-south-1' }
+
+      }));
+
+      testingStage.addPost(new ManualApprovalStep('approval'));
+
+      const prodStage=democicdpipeline.addStage(new PipelineAppStage(this,'prod', {
+        env: { account: '715841370050', region: 'ap-south-1' }
+
+      }));
         
       
   }
